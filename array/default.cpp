243af6d21cf6a1a554b91879a90beebc5418ca1b
cottonstonne <cottonstonne@gmail.com>
2022-04-06 17:07:34 +0530

    two

---
+++ array/default.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int arr[5][5]={0};
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++){
+			cout << arr[i][j] << endl;
+		}
+	}
+	return 0;
+}
