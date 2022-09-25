b191558bed9ed1ecf0ca29c6fe393c41d70ab2fd
shyam <shyam@pop-os.localdomain>
2022-09-25 23:20:25 +0530

    veckey

---
+++ hashing/veckey.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	map<vector<int>,int>store;
+	set<vector<int>>store1;
+	//cannot create unordered_map bcoz,a vaild hash could not be created
+	//unordered_map<vector<int>,int>store1;
+	//unordered_set<vector<int>>store2;
+	return 0;
+}
